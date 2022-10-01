ae6ccbe059e2668b16966c23e508f55a532e49df
Prateek-Srivastava1001 <56780232+Prateek-Srivastava1001@users.noreply.github.com>
2022-10-01 08:12:47 +0530

    Added Residential customer data acquisition

--- frontend.py
+++ frontend.py
@@ -26,3 +26,78 @@ osm = OSM(fp)
 # =======================
 drive_net = osm.get_network(network_type="driving")
 drive_net.plot()
+
+######################################### RISHIKA ##########################
+import osmium as osm
+import pandas as pd
+
+class OSMHandler(osm.SimpleHandler):
+    def __init__(self):
+        osm.SimpleHandler.__init__(self)
+        self.osm_data = []
+        self.node_data = []
+        self.i = []
+    def tag_inventory(self, elem, elem_type):
+        # if self.i == 0 and elem_type=="way":
+        #     print(dir(elem))
+        #     self.i = 1
+        for tag in elem.tags:
+            if tag.k=="building" and (tag.v=="yes" or tag.v=="residential"):
+                self.osm_data.append([elem_type, 
+                                    elem.id, 
+                                    elem.version,
+                                    elem.visible,
+                                    pd.Timestamp(elem.timestamp),
+                                    elem.uid,
+                                    elem.user,
+                                    elem.changeset,
+                                    len(elem.tags),
+                                    tag.k, 
+                                    tag.v])
+                nd = []
+                for n in elem.nodes:
+                    nd.append(n.ref)
+                self.i.append(nd)
+                
+
+    def node(self, n):
+        self.node_data.append([n.id, n.location.lat, n.location.lon])
+        self.tag_inventory(n, "node")
+
+    def way(self, w):
+        self.tag_inventory(w, "way")
+        # print(w.nd)
+
+    def relation(self, r):
+        self.tag_inventory(r, "relation")
+
+
+oh = OSMHandler()
+oh.apply_file("chamba.osm")
+
+data_colnames = ['type', 'id', 'version', 'visible', 'ts', 'uid',
+                 'user', 'chgset', 'ntags', 'tagkey', 'tagvalue']
+df_osm = pd.DataFrame(oh.osm_data, columns=data_colnames)
+node_colnames = ['id', 'latitude', 'longitude']
+df_node = pd.DataFrame(oh.node_data, columns = node_colnames)
+
+# print(df_node)
+# print(df_osm)
+resi_lat = []
+resi_lon = []
+for x in oh.i:
+    lat = 0
+    lon = 0
+    idx = 0
+    for y in x:
+        # print(df_node['latitude'].where(df_node['id']==y).dropna())
+        lat += df_node['latitude'].where(df_node['id']==y).dropna().tolist()[0]
+        lon += df_node['longitude'].where(df_node['id']==y).dropna().tolist()[0]
+        idx = idx+1
+    lat = lat/idx
+    lon = lon/idx
+    resi_lat.append(round(lat,6))
+    resi_lon.append(round(lon,6))
+
+print(resi_lat)
+print(resi_lon)
